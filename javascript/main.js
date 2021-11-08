5bb8c132db50e69e29417512dcbb5a3f62472822
jwmenjivar <jwmenjivar@gmail.com>
2021-11-08 00:31:25 -0800

    script draws when mousing over

--- javascript/main.js
+++ javascript/main.js
@@ -4,11 +4,31 @@ document.body.appendChild(container)
 for(let i=1; i<=256;i++){
 let first = document.createElement('div');
 container.appendChild(first)
-first.textContent="txt"+i;
+//first.textContent="txt"+i;
+first.style.height="40px"
 first.style.border="1px solid blue"
-first.style.margin="10px"
+first.style.margin=""
+first.className="child"
+first.id=`square${i}`
 }
 container.style.display="grid";
-container.style.gridTemplateColumns="repeat(16, 80px)"
+container.style.gridTemplateColumns="repeat(16, 40px)"
 }
 divCreator();
+
+function addClassAndColor(){
+    //console.log(this.id)
+let selected = document.querySelector(`#${this.id}`);
+selected.classList.add("colored"); 
+}
+
+let divs = document.querySelectorAll('.child');
+divs.forEach(div =>  div.addEventListener('mouseover', addClassAndColor))
+
+/* function classNameToggler(div){
+    div.classList.add("hovered");
+}
+
+let alldivs = document.querySelectorAll('.child');
+
+alldivs.forEach */
